d8f8d65c01e3b78c526a508d6a71afb1f14f690e
Kendal-ming <2929890969@qq.com>
2024-09-16 11:43:15 +0800

    Site updated: 2024-09-16 11:43:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/15/hello-world/","2024/09/15/LearnGit/"];function toRandomPost(){
+var posts=["2024/09/15/LearnGit/","2024/09/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
