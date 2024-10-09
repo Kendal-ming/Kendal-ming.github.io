8c05e8ce425a072c2ac385c795bdf7341fc5e426
Kendal-ming <2929890969@qq.com>
2024-10-09 22:15:08 +0800

    Site updated: 2024-10-09 22:15:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/15/LearnGit/","2024/10/07/beautyofcontrol/","2024/09/15/hello-world/","2024/10/09/life1008/","2024/10/08/mathformula/","2024/10/09/paper1/"];function toRandomPost(){
+var posts=["2024/09/15/LearnGit/","2024/10/08/mathformula/","2024/09/15/hello-world/","2024/10/07/beautyofcontrol/","2024/10/09/paper1/","2024/10/09/life1008/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
